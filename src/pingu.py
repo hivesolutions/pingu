9b7d143482646d567d0374f9d08eb57429e009c2
joamag <joamag@gmail.com>
2012-11-28 19:37:23 +0000

    removed unused code

--- src/pingu.py
+++ src/pingu.py
@@ -111,9 +111,6 @@ HEADERS = {
 http client to use """
 
 app = flask.Flask(__name__)
-#app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(365)
-#app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-#app.config["MAX_CONTENT_LENGTH"] = 1024 ** 3
 
 mongo_url = os.getenv("MONGOHQ_URL", MONGO_URL)
 quorum.mongo.url = mongo_url
