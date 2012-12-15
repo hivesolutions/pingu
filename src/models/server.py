b1b82cdb10ec4ccdf7d8a0b1b992b90c6dfa1e83
joamag <joamag@gmail.com>
2012-12-15 19:13:39 +0000

    new server model added

--- src/models/server.py
+++ src/models/server.py
@@ -19,6 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Hive Pingu System. If not, see <http://www.gnu.org/licenses/>.
 
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
 __version__ = "1.0.0"
 """ The version of the module """
 
@@ -34,12 +37,7 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-import account
 import base
-import contact
-import server
 
-from account import *
-from base import *
-from contact import *
-from server import *
+class Server(base.Base):
+    pass
